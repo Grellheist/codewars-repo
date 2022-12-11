96a796e6a5737a204cfe2fc833ce18d1c387d12c
grellheist <grellheist@tutanota.com>
2022-12-10 23:21:33 -0300

    Updated

--- 5 kyu/rgb_to_hex.py
+++ 5 kyu/rgb_to_hex.py
@@ -19,3 +19,9 @@ def rgb(r, g, b):
         if len(colors) == 1: result[index] = '0' + result[index]
         index += 1
     return ''.join(result)
+
+#Better solution:
+#
+#def rgb(r, g, b):
+#    round = lambda x: min(255, max(x, 0))
+#    return ("{:02X}" * 3).format(round(r), round(g), round(b))
