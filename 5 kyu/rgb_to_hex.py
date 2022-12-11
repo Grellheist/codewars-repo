43cb41f61d748c547f7e93835fff42ad328300c8
grellheist <grellheist@tutanota.com>
2022-12-10 23:10:25 -0300

    Updated

--- 5 kyu/rgb_to_hex.py
+++ 5 kyu/rgb_to_hex.py
@@ -1,3 +1,21 @@
 def rgb(r, g, b):
-    
-    return result
+    digits = '0123456789ABCDEF'
+    result = []
+    start_values = [r, g, b]
+    for colors in start_values:
+        color_values = ''
+        if colors > 255:
+            result.append('FF')
+        else:
+            while colors > 0:
+                color_values = digits[colors % 16] + color_values
+                colors = colors // 16
+            result.append(color_values)
+
+    index = 0
+
+    for colors in result:
+        if len(colors) == 0: result[index] = '00'
+        if len(colors) == 1: result[index] = '0' + result[index]
+        index += 1
+    return ''.join(result)
